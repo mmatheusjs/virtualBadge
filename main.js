61ef962c51467a8677b3b37bfaa2d08cf5b01d7a
mmatheusjs <math.heenrique@live.com>
2021-10-23 09:01:26 -0300

    First commit of virtualBadge

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'mmatheusjs',
+  youtube: 'maykbrito',
+  facebook: 'matheus.oliveirahenrique.3',
+  instagram: 'mmatheusjs',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of linkList.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userImageProfile.src = data.avatar_url
+      userName.textContent = `${data.name}`
+      userLink.href = data.html_url
+      githubLogin.textContent = data.githubLogin
+      userBio.textContent = data.bio
+    })
+}
+
+getGitHubProfileInfos()
